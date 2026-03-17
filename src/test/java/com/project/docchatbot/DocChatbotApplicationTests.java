7ca9adcccb24d587a3f975ab5068176114cd143e
Koohyemin <ptkf1023@naver.com>
2026-03-17 20:33:11 +0900

    chore: DocChatbotApplication 클래스 누락 파일 추가

---
+++ src/test/java/com/project/docchatbot/DocChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.docchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
